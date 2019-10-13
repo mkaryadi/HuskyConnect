ec230d752051a79b8010982e18c1663c4eb58dd2
johnli01 <john.li9631@gmail.com>
2019-10-13 00:14:59 -0700

    tweaking words

--- docs/script.js
+++ docs/script.js
@@ -1,13 +1,14 @@
 const db = firebase.firestore();
 
 function writeData() {
-    db.collection('users').doc('firstUser').set({
-        name:'Jimmy Smith'
+    db.collection("user").doc("personOne").set({
+        name: "Jimmy Smith",
+        contactInfo: "JimmySmith@uw.edu"
     })
-    .then(function(){
-        console.log('data written');
+    .then(function() {
+        console.log("Document successfully written!");
     })
     .catch(function(error){
-        console.error('Error adding document', error);
+        console.error("Error adding document: ", error);
     });
 }
