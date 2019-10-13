068e43630a63b58b5d7fd1f221cccd590f479f5e
johnli01 <john.li9631@gmail.com>
2019-10-12 21:20:18 -0700

    Implemented hasNext for user

--- BackEnd/User.java
+++ BackEnd/User.java
@@ -1,31 +1,12 @@
 import java.util.*;
 
 public class User {
-<<<<<<< HEAD
-
-    private String name;
-    private String email;
-    private Set<String> taken;
-    private Set<String> taking;
-    
-    public User() {
-        Scanner console = new Scanner(System.in);
-        System.out.
-        taken = new TreeSet<>();
-        taking = new TreeSet<>();
-        System.out.println("Enter classes you've taken?");
-        fill(taken, pastPrompt);
-        String currentPrompt = "Enter classes you're taking?"
-        fill(taking, currentPrompt);
-    }
-=======
     private Scanner console;
     private Set<String> taken;
     private Set<String> taking;
     
     private String name;
     private String contactInfo;
->>>>>>> b154c52356a5cb06a3d3f8e518dbb1dcd52a12dc
     
     public User(String name, String contactInfo) {
         console = new Scanner(System.in);
@@ -50,17 +31,11 @@ public class User {
             done = true;
         } else {
             data.add(course);
-<<<<<<< HEAD
-            System.out.println("Is that all? If so, please type the number 1. Otherwise, type any other number.");
-            int checkNum = console.nextInt();
-            if (checkNum == 1) { 
-=======
         }
         while (!done) {
             System.out.println("Enter another course (Q to Quit)");
             course = console.nextLine();
             if (course.equals("q") || course.equals("Q")) {
->>>>>>> b154c52356a5cb06a3d3f8e518dbb1dcd52a12dc
                 done = true;
             } else {
                data.add(course);
@@ -82,5 +57,9 @@ public class User {
     public Set<String> compare(Set<String> personOne, Set<String> personTwo) {
          Set<String> similarCourses = new TreeSet<String>();
          return similarCourses;
+    }
+    
+    public boolean hasTaken(String course) {
+         return taken.contains(course);
     }
 }
--- BackEnd/UserClient.java
+++ BackEnd/UserClient.java
@@ -2,6 +2,8 @@ public class UserClient {
 
     public static void main(String[] args) {
         User personOne = new User("Jack", "jacklee7@uw.edu");
-        personOne.printTakingList();
+        personOne.printTakenList();
+        
+        System.out.println(personOne.hasTaken("CSE142"));
     }
 }
