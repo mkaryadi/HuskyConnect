ee2abcada41e1bdd2247457799368c0b4985e901
johnli01 <john.li9631@gmail.com>
2019-10-12 21:32:30 -0700

    Cleaning up some code

--- BackEnd/User.java
+++ BackEnd/User.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 public class User {
 
+    private Scanner console;
     private Set<String> taken;
     private Set<String> taking;
     
     public User() {
+        console = new Scanner(System.in);
         taken = new TreeSet<>();
         taking = new TreeSet<>();
         System.out.println("Enter classes you've taken?");
@@ -13,11 +15,8 @@ public class User {
         String currentPrompt = "Enter classes you're taking?");
         fill(taking, currentPrompt);
     }
-    
-    
      
     private void fill(Set<String> data, String prompt) {
-        Scanner console = new Scanner(System.in);
         boolean done = false;
         while (!done) {
             System.out.println(prompt);
